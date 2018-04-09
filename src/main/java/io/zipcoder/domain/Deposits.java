6fc2c054ce28dc830ecc6afc3cbe579aa4c0726e
Haysel Santiago <hayselsantiago@zipcoders-MacBook-Pro-67.local>
2018-04-09 11:40:58 -0400

    Added withrawal/deposit POJO

--- src/main/java/io/zipcoder/domain/Deposits.java
+++ src/main/java/io/zipcoder/domain/Deposits.java
@@ -1,4 +1,76 @@
 package io.zipcoder.domain;
 
 public class Deposits {
+    private long id;
+    private String type;
+    private String transaction_date;
+    private String status;
+    private long payer_id;
+    private String medium;
+    private double amount;
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTransaction_date() {
+        return transaction_date;
+    }
+
+    public void setTransaction_date(String transaction_date) {
+        this.transaction_date = transaction_date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public long getPayer_id() {
+        return payer_id;
+    }
+
+    public void setPayer_id(long payer_id) {
+        this.payer_id = payer_id;
+    }
+
+    public String getMedium() {
+        return medium;
+    }
+
+    public void setMedium(String medium) {
+        this.medium = medium;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
