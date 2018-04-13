c26db68952b827a6827e6db9c5f793a66f2da0af
April Rivera <aprilrivera@zipcoders-MacBook-Pro-2.local>
2018-04-13 09:09:26 -0400

    finally merged i think

--- src/main/java/io/zipcoder/domain/Deposits.java
+++ src/main/java/io/zipcoder/domain/Deposits.java
@@ -1,4 +1,98 @@
 package io.zipcoder.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Deposits {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String type;
+    private String transaction_date;
+    private String status;
+    private long payer_id;
+    private String medium;
+    private double amount;
+    private String description;
+
+    public Deposits(String type, String transaction_date, String status, long payer_id, String medium, double amount, String description) {
+        this.type = type;
+        this.transaction_date = transaction_date;
+        this.status = status;
+        this.payer_id = payer_id;
+        this.medium = medium;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public Deposits() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTransaction_date() {
+        return transaction_date;
+    }
+
+    public void setTransaction_date(String transaction_date) {
+        this.transaction_date = transaction_date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public long getPayer_id() {
+        return payer_id;
+    }
+
+    public void setPayer_id(long payer_id) {
+        this.payer_id = payer_id;
+    }
+
+    public String getMedium() {
+        return medium;
+    }
+
+    public void setMedium(String medium) {
+        this.medium = medium;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/io/zipcoder/domain/Withdrawal.java
+++ src/main/java/io/zipcoder/domain/Withdrawal.java
@@ -1,4 +1,100 @@
 package io.zipcoder.domain;
 
+import com.sun.javafx.beans.IDProperty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Withdrawal {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String type;
+    private String transaction_date;
+    private String status;
+    private long payee_id;
+    private String medium;
+    private double amount;
+    private String description;
+
+    public Withdrawal(String type, String transaction_date, String status, long payee_id, String medium, double amount, String description) {
+        this.type = type;
+        this.transaction_date = transaction_date;
+        this.status = status;
+        this.payee_id = payee_id;
+        this.medium = medium;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public Withdrawal() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTransaction_date() {
+        return transaction_date;
+    }
+
+    public void setTransaction_date(String transaction_date) {
+        this.transaction_date = transaction_date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public long getPayer_id() {
+        return payee_id;
+    }
+
+    public void setPayer_id(long payer_id) {
+        this.payee_id = payer_id;
+    }
+
+    public String getMedium() {
+        return medium;
+    }
+
+    public void setMedium(String medium) {
+        this.medium = medium;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
